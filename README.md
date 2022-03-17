59c6fd0be225495e8c6b058f85d8f25ec003c812
DonnieDice <DonnieDice@ProtonMail.com>
2022-03-17 02:34:02 -0500

    v1.0.2
    Muted WoW default level up sound

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-WC3LU
+
+WC3LU - Warcraft 3 Level Up!
+
+
+This is an addon that will play the "LEVEL UP!" chime from Warcraft 3 when you level up in WoW.
+
+
+This addon works in SHADOWLANDS!
+
+This addon works in CLASSIC WOW!
+
+This addon works in CLASSIC TBC!
+
+
+v1.0.2
+
+Added - Muted WoW default level up sound
---
+++ main.lua
@@ -0,0 +1,15 @@
+-- Event function level up
+local a = CreateFrame("Frame")
+a:RegisterEvent("PLAYER_LEVEL_UP")
+a:SetScript("OnEvent",
+	function(self, event, ...)
+		PlaySoundFile("Interface\\Addons\\WC3LU\\WC3LU.ogg", "Master")
+	end)
+
+-- Mute default level up sound
+local b = CreateFrame("Frame")
+b:RegisterEvent("ADDON_LOADED")
+b:SetScript("OnEvent",
+	function(self, event, ...)
+		MuteSoundFile(569593)
+	end)
